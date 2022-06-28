46b93a995a69ee955364683cbd134423984e1ce5
CHAITANYA735 <97425744+CHAITANYA735@users.noreply.github.com>
2022-06-28 20:12:56 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,arr[100],i,sum1=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             sum1=sum1+arr[i];         }     }     printf("%d",sum1); }

---
+++ Even_Sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,arr[100],i,sum1=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            sum1=sum1+arr[i];
+        }
+    }
+    printf("%d",sum1);
+}
